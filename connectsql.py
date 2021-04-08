2ee0671a515fe1696fce7a97d76fd85e8614bf2f
oritn <orit0261>
2021-04-08 13:10:00 +0300

    update

--- connectsql.py
+++ connectsql.py
@@ -1,69 +1,55 @@
-import csv
-import datetime
-import itertools
 import json
 
-import mysql.connector
+from config import connect
+import csv
+import datetime
 import requests
 
-db_connection = mysql.connector.connect(
-    host="104.197.7.195",
-    user="interview_user",
-    passwd="zz!!@@202111558843",
-    database="interview")
 
-print(db_connection)
-# creating database_cursor to perform SQL operation
-db_cursor = db_connection.cursor()
-
-# # Query for creating table
-# ArtistTableSql = """CREATE TABLE orbital_data_orit_naor(
-# ID INT(20) PRIMARY KEY AUTO_INCREMENT,
-# CITY  CHAR(100) NOT NULL,
-# DURATION INT,
-# RISETIME datetime)"""
-# db_cursor.execute(ArtistTableSql)
-# db_connection.close()
-
-# student_sql_query = "INSERT INTO orbital_data_orit_naor(city,duration) VALUES('ash',01)"
-# db_cursor.execute(student_sql_query)
-
-# student_sql_query = "INSERT INTO student(id,name) VALUES(01, 'John')"
-# employee_sql_query = " INSERT INTO employee (id, name, salary) VALUES (01, 'John', 10000)"
-# #Execute cursor and pass query as well as student data
-# db_cursor.execute(student_sql_query)
-# #Execute cursor and pass query of employee and data of employee
-# 	db_cursor.execute(employee_sql_query)
-# db_connection.commit()
-# print(db_cursor.rowcount, "Record Inserted")
+def fill_json_to_tbl():
+    with open('locations.json', 'r') as j:
+        json_data = json.load(j)
+        return [(get_response(item['name'], item['latitude'], item['longitude'], 50)) for item in
+                json_data['locations']]
 
 
-# queries for retrievint all rows
-select_sql = "Select id,city,duration,CAST(risetime AS char) as risktime from orbital_data_orit_naor;"
-db_cursor.execute(select_sql)
+def get_response(p_city, p_lat, p_lon, p_n):
+    db_connection, db_cursor = connect()
+    params = {'lat': p_lat, 'lon': p_lon, 'n': p_n}
+    response = requests.get("http://api.open-notify.org/iss-pass.json", params=params)
+    try:
+        response_json = response.json()['response']
+        data = [(p_city, response_json['duration'], datetime.datetime.fromtimestamp(int(response_json["risetime"])))
+                for response_json in response.json()['response']]
+        db_cursor.executemany("INSERT INTO orbital_data_orit_naor(CITY,DURATION,RISETIME) VALUES (%s,%s,%s)", data)
+        db_connection.commit()
+    except Exception as e:
+        print(e, flush=True)
+        db_connection.rollback()
+    finally:
+        db_cursor.close()
+        db_connection.close()
 
-rows = db_cursor.fetchall()
-column_names = [i[0] for i in db_cursor.description]
-fp = open('test.csv', 'w')
-myFile = csv.writer(fp, lineterminator='\n')
-myFile.writerow(column_names)
-myFile.writerows(rows)
-fp.close()
 
-# commiting the connection then closing it.
-db_connection.commit()
+def sql_to_csv():
+    db_connection, db_cursor = connect()
+    try:
+        db_cursor.callproc('city_status_orit_naor')
+        for result in db_cursor.stored_results():
+            res1=result.fetchall()
+            column_names = [x[0] for x in result.description]
 
-params = {'lat': 32.8000, 'lon': 34.9833, 'n': 50}
-response = requests.get("http://api.open-notify.org/iss-pass.json", params=params)
-row = response.json()['response']
-try:
-    data = [('city',row['duration'],datetime.datetime.fromtimestamp(int(row["risetime"]))) for row in response.json()['response']]
-    db_cursor.executemany("INSERT INTO orbital_data_orit_naor(CITY,DURATION,RISETIME) VALUES (%s,%s,%s)", data)
-    db_connection.commit()
-except Exception as e:
-    print(e, flush=True)
-    db_connection.rollback()
-finally:
-  db_connection.close()
-# all = response.json()['response'] # No need to parse this, unless you want to check it's valid
-# db_cursor.execute('insert into t select * from json_populate_recordset(null::t, %s)', [all])
+        response_jsons = res1
+        fp = open('results.csv', 'w')
+        myFile = csv.writer(fp, lineterminator='\n')
+        myFile.writerow(column_names)
+        myFile.writerows(response_jsons)
+        fp.close()
+    except Exception as e:
+        print(e, flush=True)
+    finally:
+        db_cursor.close()
+        db_connection.close()
+
+
+sql_to_csv()
--- main.py
+++ main.py
@@ -7,6 +7,4 @@ params = {'lat':32.8000,'lon':34.9833,'n':50}
 response = requests.get("http://api.open-notify.org/iss-pass.json",params=params)
 rows = response.json()['response']
 
-all = response.text # No need to parse this, unless you want to check it's valid
-cur.execute('insert into t select * from json_populate_recordset(null::t, %s)', [all])
 
